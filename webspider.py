3b81bd14047951d453789c3631ce079d551aea37
Sunidhi Dhawan <dhawan.sunidhi10@gmail.com>
2021-10-10 00:33:07 -0400

    FINAL WEB SCRAPER

--- webspider.py
+++ webspider.py
@@ -5,14 +5,15 @@ import requests
 from selenium import webdriver
 import os
 import io
-from google.cloud import vision
+
+# from google.cloud import vision
 
 DRIVER_PATH = '/Users/sunidhidhawan/Desktop/ai-perlapse/chromedriver'
 wd = webdriver.Chrome(executable_path=DRIVER_PATH)
 wd.get('https://google.com')
 
 
-def persist_image(folder_path: str, url: str):
+def persist_image(folder_path: str, url: str, unique_file_number: int):
     try:
         image_content = requests.get(url).content
 
@@ -22,7 +23,7 @@ def persist_image(folder_path: str, url: str):
     try:
         image_file = io.BytesIO(image_content)
         image = Image.open(image_file).convert('RGB')
-        file_path = os.path.join(folder_path, hashlib.sha1(image_content).hexdigest()[:10] + '.jpg')
+        file_path = os.path.join(folder_path, 'image' + str(unique_file_number) + '.jpg')
         with open(file_path, 'wb') as f:
             image.save(f, "JPEG", quality=85)
         print(f"SUCCESS - saved {url} - as {file_path}")
@@ -87,7 +88,7 @@ def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_b
     return image_urls
 
 
-def search_and_download(search_term: str, driver_path: str, target_path='./images', number_images: int):
+def search_and_download(search_term: str, driver_path: str, number_images: int, target_path='./images'):
     target_folder = os.path.join(target_path, '_'.join(search_term.lower().split(' ')))
 
     if not os.path.exists(target_folder):
@@ -95,13 +96,15 @@ def search_and_download(search_term: str, driver_path: str, target_path='./image
 
     with webdriver.Chrome(executable_path=driver_path) as wd:
         res = fetch_image_urls(search_term, number_images, wd=wd, sleep_between_interactions=0.5)
-
+    unique_file_number = 1
     for elem in res:
-        persist_image(target_folder, elem)
+        persist_image(target_folder, elem, unique_file_number)
+        unique_file_number += 1
 
 
+"""
 def detect_landmarks(path, landmark_name):
-    """ Detects landmarks in the file. """
+    Detects landmarks in the file. 
 
     os.environ[
         'GOOGLE_APPLICATION_CREDENTIALS'] = r'/Users/pats/OneDrive/My Stuff/VandyHacks/ai-perlapse/ServiceAccountKey.json'
@@ -126,8 +129,8 @@ def detect_landmarks(path, landmark_name):
                 # os.remove(filename)
         except:
             print("NO LANDMARKS:" + filename)
+"""
 
-
-search_term = input("Name of Monument:" )
-number_img = input("Number of images for hyperlapse: ")
-search_and_download(search_term=search_term, driver_path=DRIVER_PATH, number_images=number_img*2)
+search_term = input("Name of Monument:")
+number_img = int(input("Number of images for hyperlapse: "))
+search_and_download(search_term=search_term, driver_path=DRIVER_PATH, number_images=number_img * 2)
